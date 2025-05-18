4abecef7877dd7679da26b915d79eb04fcf7f61f
Alvaro Franqueira <franqueira.alvaro@gmail.com>
2025-05-18 02:53:37 +0200

    push sub casino

---
+++ frontend/react-casino-roulette/src/helpers/getWheelNumbers/getWheelNumbers.ts
@@ -0,0 +1,8 @@
+import { getRandomArrayItem } from '../../utills';
+
+export const getWheelNumbers = () => {
+  // American roulette numbers (0, 00, and 1-36)
+const wheelNumbers = ["28", "9", "26", "30", "11", "7", "20", "32", "17", "5", "22", "34", "15", "3", "24", "36", "13", "1", "00", "27", "10", "25", "29", "12", "8", "19", "31", "18", "6", "21", "33", "16", "4", "23", "35", "14", "2","0"];
+
+  return wheelNumbers;
+};
